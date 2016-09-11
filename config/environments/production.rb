85f5c257b2fad47e556e76e572e7cd787c13c898
LeChin <elohcin@gmail.com>
2016-09-11 14:39:50 -0700

    update paperclip, add example env for maintaanenace

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,10 +87,10 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: ENV['S3_BUCKET'],
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-    },
-    s3_region: 'us-west-2'
+      bucket: ENV.fetch('AWS_S3_BUCKET'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_S3_REGION')
+    }
   }
 end
