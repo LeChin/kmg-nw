23b2e87445b5c4b74b7ca35c78c1cdd2533f190b
LeChin <elohcin@gmail.com>
2016-09-11 14:56:55 -0700

    use paperclip style image uploads

--- app/models/home_gallery_item.rb
+++ app/models/home_gallery_item.rb
@@ -3,14 +3,18 @@ class HomeGalleryItem < ApplicationRecord
 
   belongs_to :static_page, class_name: 'Fae::StaticPage'
 
-  has_one :image, -> { where(attached_as: 'image') },
-    as: :imageable,
-    class_name: '::Fae::Image',
-    dependent: :destroy,
+  # has_one :image, -> { where(attached_as: 'image') },
+  #   as: :imageable,
+  #   class_name: '::Fae::Image',
+  #   dependent: :destroy
+  # accepts_nested_attributes_for :image, allow_destroy: true
+
+  has_attached_file :image,
     storage: :s3,
     s3_credentials: "#{Rails.root}/config/aws.yml",
     bucket: 'kmg-nw-01'
-  accepts_nested_attributes_for :image, allow_destroy: true
+
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   scope :live, -> { where(live: true) }
 
