28ba759b1b26fa9f1e9b3b67f9f7bd0c694d907c
LeChin <elohcin@gmail.com>
2016-09-11 15:01:50 -0700

    revert changes for now and sit on aws stuff

--- app/models/home_gallery_item.rb
+++ app/models/home_gallery_item.rb
@@ -3,18 +3,11 @@ class HomeGalleryItem < ApplicationRecord
 
   belongs_to :static_page, class_name: 'Fae::StaticPage'
 
-  # has_one :image, -> { where(attached_as: 'image') },
-  #   as: :imageable,
-  #   class_name: '::Fae::Image',
-  #   dependent: :destroy
-  # accepts_nested_attributes_for :image, allow_destroy: true
-
-  has_attached_file :image,
-    storage: :s3,
-    s3_credentials: "#{Rails.root}/config/aws.yml",
-    bucket: 'kmg-nw-01'
-
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  has_one :image, -> { where(attached_as: 'image') },
+    as: :imageable,
+    class_name: '::Fae::Image',
+    dependent: :destroy
+  accepts_nested_attributes_for :image, allow_destroy: true
 
   scope :live, -> { where(live: true) }
 
