abdf436570c844848b9ebe4e7d7d91069165b911
LeChin <elohcin@gmail.com>
2016-09-11 18:27:58 -0700

    fog update to hopefully fix error

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'us-west-2' ,
+      region: ENV['AWS_S3_REGION'],
       path_style: true
     }
     config.storage :fog 
