3b8575fade324357af6561e4e1431aa125aca2f1
LeChin <elohcin@gmail.com>
2016-09-21 21:40:15 -0700

    scope workouts on sitemap

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,7 +14,7 @@ class PagesController < ApplicationController
   def sitemap
     @page_title = 'Sitemap'
     @sitemap_news_items = NewsItem.live.ordered_by_date
-    @sitemap_workouts = Workout.live
+    @sitemap_workouts = Workout.for_landing
     @sitemap_instructors = Instructor.live
 
     respond_to do |format|
